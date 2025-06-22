2f8e5598954dbf8a6014bf9207e6c3fd216da0fe
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-06-22 23:11:43 +0530

    GRAD-CAM API & UI

--- server/index.js
+++ server/index.js
@@ -5,8 +5,14 @@ import reviewRoutes from './routes/review.js';
 import logoRoutes from './routes/logoCheck.js';
 import graphRoutes from './routes/graph.js';
 import ocrRoutes from './routes/ocrMatch.js';
+import gradCamRoutes from './routes/gradCam.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config({path: './.env'})
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,10 +20,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use('/gradcam', express.static(path.join(__dirname, '../data/gradcam_outputs')));
+
 app.use('/api/review', reviewRoutes);
 app.use('/api/logo_check', logoRoutes);
 app.use('/api/graph', graphRoutes);
 app.use('/api/ocr_match', ocrRoutes)
+app.use('/api/predict-authenticity', gradCamRoutes)
 
 app.get('/', (req, res) => {
     res.send('TrustSphere Backend is running');
