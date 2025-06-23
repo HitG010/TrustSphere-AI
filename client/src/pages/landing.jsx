ab0ecf3def3ef16b9b4e20315b47a45d01208397
kartikbindra <kartikbindra_23cs211@dtu.ac.in>
2025-06-23 12:13:47 +0530

    Architecture added

--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LandingNavbar from '../components/LandingNavbar';
 import trustLandingBg from '../assets/trustLandingBg.png';
 import trustLandingTileFeatures from '../assets/trustLandingTileFeatures.png';
+import architecture_Trustsphere from '../assets/architecture_TrustSphere.png';
 import { Link } from 'react-router-dom';
 import { FaArrowRight, FaGithub } from 'react-icons/fa6';
 
@@ -21,11 +22,14 @@ function landing() {
                     </p>
                     <div className="mt-8 flex items-center justify-center gap-2">
                         <Link to="https://github.com/HitG010/TrustSphere-AI" className="rounded-md border border-white/25 text-white px-4 py-2 rounded flex items-center gap-2 w-fit justify-center hover:bg-white/15 transition-all duration-300"><FaGithub className='w-4 h-4' /> Github</Link>
-                        <Link to="https://github.com/HitG010/TrustSphere-AI" className="rounded-md border border-white/25 text-black bg-white px-4 py-2 rounded  flex items-center gap-2 w-fit justify-center hover:bg-white/65 transition-all duration-300"> Get Started <FaArrowRight className='w-4 h-4' /></Link>
+                        <Link to="/trustsphere/reviewAnalyzer" className="rounded-md border border-white/25 text-black bg-white px-4 py-2 rounded  flex items-center gap-2 w-fit justify-center hover:bg-white/65 transition-all duration-300"> Get Started <FaArrowRight className='w-4 h-4' /></Link>
                     </div>
                 </div>
                 <div className='mt-[-100px] flex flex-col items-center justify-center h-screen font-semibold flex-shrink-0'>
                     <img src={trustLandingTileFeatures} alt="TrustSphere AI Features" className='scale-45 h-auto rounded-lg' />
+                </div>
+                <div className='mt-[-100px] flex flex-col items-center justify-center h-screen font-semibold flex-shrink-0'>
+                    <img src={architecture_Trustsphere} alt="TrustSphere AI Workflow" className='scale-65 h-auto rounded-lg' />
                 </div>
                 <footer className="w-full py-4 bg-white/10 border-t border-white/20 mt-auto flex flex-col items-center">
                     <div className="text-white/70 text-sm font-light flex flex-col items-center gap-1">
